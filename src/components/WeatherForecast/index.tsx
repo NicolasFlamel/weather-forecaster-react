b0fabe6d3bd99dc755255f5f2dd2d79ac48a56b9
Amauri <hamauri@aol.com>
2024-02-23 12:16:22 -0800

    implement dayjs for forecast days

--- src/components/WeatherForecast/index.tsx
+++ src/components/WeatherForecast/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import dayjs from 'dayjs';
 import { useWeather } from 'context/WeatherContext';
 import { ForecastCard } from 'components';
 
@@ -16,22 +17,24 @@ const WeatherForecast = () => {
       <h1>Forecast</h1>
       <ol className="forecast-list">
         {forecastArray?.map((forecast) => {
+          const forecastDate = dayjs(forecast.dt_txt);
           const tempConverted = ((forecast.main.temp - 273.15) * 9) / 5 + 32;
           const speedConverted = forecast.wind.speed * 2.237;
 
           return (
             <li key={forecast.dt}>
               <ForecastCard>
-                <p>Date: {forecast.dt_txt}</p>
                 <img
                   className="weather-img weather-icon"
                   title={forecast.weather[0].description}
                   alt={forecast.weather[0].description}
                   src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`}
                 />
+                <p>{forecastDate.format('dddd')}</p>
                 <p>Temperature: {tempConverted.toFixed(2) + ' °F'}</p>
                 <p>Wind Speed: {speedConverted.toFixed(2) + ' MPH'}</p>
                 <p>Humidity: {forecast.main.humidity + '%'}</p>
+                <p>Precipitation: {forecast.pop * 100}%</p>
               </ForecastCard>
             </li>
           );
